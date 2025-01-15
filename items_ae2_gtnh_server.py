a0fcd7e64143c535fbdd7ffcc7f6c58e1884242f
ArthurTakita <44661117+ArthurTakita@users.noreply.github.com>
2025-01-15 16:53:20 -0300

    Update items_ae2_gtnh_server.py

--- items_ae2_gtnh_server.py
+++ items_ae2_gtnh_server.py
@@ -3,16 +3,10 @@ import streamlit as st
 from st_supabase_connection import SupabaseConnection, execute_query
 
 # Initialize connection.
-#conn = st.connection("supabase",type=SupabaseConnection)
-
-st_supabase_client = st.connection(
-    name="YOUR_CONNECTION_NAME",
-    type=SupabaseConnection,
-    ttl=None,
-)
+conn = st.connection("supabase",type=SupabaseConnection)
 
 # Perform query.
-rows = execute_query(st_supabase_client.table("items_ae2").select("*"), ttl=0)
+rows = execute_query(conn.table("items_ae2").select("*"), ttl=0)
 
 # Print results.
 for row in rows.data:
