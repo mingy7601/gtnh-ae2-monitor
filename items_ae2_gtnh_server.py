489a155516368a8517583001bb95b846b2675560
ArthurTakita <44661117+ArthurTakita@users.noreply.github.com>
2025-01-15 16:37:30 -0300

    Update items_ae2_gtnh_server.py

--- items_ae2_gtnh_server.py
+++ items_ae2_gtnh_server.py
@@ -1,1 +1,16 @@
 
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="items_ae2", ttl="10m").execute()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['item']} has a :{row['quantity']}:")
+
+
+
