9bba40b5d1b52e1d8aec087a814efbcb944982c7
Arthur <takitalouro@gmail.com>
2025-02-20 09:10:51 -0300

    Ajust time

--- items_ae2_gtnh_server.py
+++ items_ae2_gtnh_server.py
@@ -49,7 +49,7 @@ for seconds in range(200):
 
     item_track = sort_table.loc[sort_table['item'] == items_filter]
     item_track['datetime'] = pd.to_datetime(item_track["datetime"])
-    last_24h = item_track[item_track["datetime"] >= datetime.datetime.now(pytz.timezone("America/Sao_Paulo")) - pd.Timedelta(days=1)]
+    last_24h = item_track[item_track["datetime"] >= pd.to_timedelta(datetime.datetime.now(pytz.timezone("America/Sao_Paulo"))) - pd.Timedelta(days=1)]
 
     if len(last_24h) <= 1:
       kpi_avg = 0
